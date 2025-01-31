7a7848deb2cdcc41c3cd3bba6db980b3dc69b23d
875188564@qq.com <a84110927>
2025-01-31 18:25:04 +0800

    test deploy

--- EKS/kubernetes.tf
+++ EKS/kubernetes.tf
@@ -3,10 +3,3 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
-resource "kubernetes_namespace" "nginx" {
-  metadata {
-    name = "nginx"
-  }
-
-}
-
