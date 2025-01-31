c1787524eb7f44767d68058e20f53313bf60b1ab
875188564@qq.com <a84110927>
2025-01-31 17:26:36 +0800

    test deploy

--- EKS/kubernetes.tf
+++ EKS/kubernetes.tf
@@ -1,57 +1,5 @@
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-resource "kubernetes_deployment" "nginx" {
-  metadata {
-    name = "nginx-deployment"
-  }
-
-  spec {
-    replicas = 2
-
-    selector {
-      match_labels = {
-        app = "nginx"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "nginx"
-        }
-      }
-
-      spec {
-        container {
-          name  = "nginx"
-          image = "nginx:latest"
-          port {
-            container_port = 80
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "nginx" {
-  metadata {
-    name = "nginx-service"
-  }
-
-  spec {
-    selector = {
-      app = "nginx"
-    }
-    port {
-      protocol    = "TCP"
-      port        = 80
-      target_port = 80
-    }
-    type = "LoadBalancer"
-  }
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
