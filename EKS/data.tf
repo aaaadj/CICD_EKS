fedf7cecfa5a16370b775b9e62a067157a45a8e8
875188564@qq.com <a84110927>
2025-01-31 17:07:22 +0800

    test deploy

--- EKS/data.tf
+++ EKS/data.tf
@@ -5,9 +5,15 @@ data "aws_availability_zones" "azs" {
 # 获取 EKS 集群的认证信息
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [
+    module.eks
+  ]
 }
 
 # 获取 EKS 集群的认证令牌
 data "aws_eks_cluster_auth" "cluster" {
-  name = data.aws_eks_cluster.cluster.name
+  name = module.eks.cluster_name
+  depends_on = [
+    module.eks
+  ]
 }
--- jenkins-server/jenkins-install.sh
+++ jenkins-server/jenkins-install.sh
@@ -34,3 +34,10 @@ else
 fi
 
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#install aws
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
