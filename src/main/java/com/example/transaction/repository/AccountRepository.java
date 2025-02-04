18bfcc14b38a04efd3027f28721e681b844d5478
875188564@qq.com <a84110927>
2025-02-05 03:28:12 +0800

    update select for update

--- src/main/java/com/example/transaction/repository/AccountRepository.java
+++ src/main/java/com/example/transaction/repository/AccountRepository.java
@@ -2,8 +2,16 @@ package com.example.transaction.repository;
 
 import com.example.transaction.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.LockModeType;
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE) // use select for update
+    @Override
+    Optional<Account> findById(String s);
 }
